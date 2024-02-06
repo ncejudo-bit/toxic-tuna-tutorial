16faa9ddf90dc0b28ac0ed3fe59cba872cc7c4a1
Nelson Aram Cejudo Solares <ncejudo@bit.lat>
2024-02-06 10:30:16 -0600

    Update
    Update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
 import svelte from "@astrojs/svelte";
 import alpinejs from "@astrojs/alpinejs";
-
 import tailwind from "@astrojs/tailwind";
+
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte(), alpinejs(), tailwind()]
+  site: "https://quiet-bienenstitch-4aa847.netlify.app",
+  integrations: [svelte(), alpinejs(), tailwind(), preact()]
 });
