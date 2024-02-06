ba1700ca54a854849f8a25f7a3e60b30d461394f
Nelson Aram Cejudo Solares <ncejudo@bit.lat>
2024-02-06 09:12:39 -0600

    Css Update
    Integration of css

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
 import svelte from "@astrojs/svelte";
-
 import alpinejs from "@astrojs/alpinejs";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte(), alpinejs()]
+  integrations: [svelte(), alpinejs(), tailwind()]
 });
